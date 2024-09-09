f8a7c581a1da52df872f949cd05611622da68052
papertots <papertots@example.com>
2024-09-09 17:20:14 -0400

    refactor Multiples

--- src/Multiples.java
+++ src/Multiples.java
@@ -1,4 +1,11 @@
-package PACKAGE_NAME;
-
 public class Multiples {
+    public static void main(String[] args) {
+        int multipleCount = 0;
+        for (int i = 1; i < 1000; i++){
+            if (i % 3 == 0 || i % 5 == 0){
+                multipleCount++;
+            }
+        }
+        System.out.println(multipleCount);
+    }
 }
